e0343b4315b6cdb494a4ba8b6dd22b6d27418dce
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-21 09:31:28 +0300

    Ensure ShoppingList component renders list of items with appropriate props

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,15 @@
 import React from "react";
-import Item from "./Item";
 
-function ShoppingList({ items }) {
+const ShoppingList = ({ items }) => {
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
-export default ShoppingList;
+export default ShoppingList;  // Default export
