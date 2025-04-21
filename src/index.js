aab0c16c4d8c7c195dc9e4eb96487d679c8d875f
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-21 09:33:07 +0300

    Render App component into DOM with index.js entry point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./index.css";
+import App from "./App";
+import "./index.css"; // Optional: only if you want styling
 
 ReactDOM.render(<App />, document.getElementById("root"));
